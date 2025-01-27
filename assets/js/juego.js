8e2bf547807bf54fa4f3229e10906a06a72ee037
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-01-27 13:27:11 -0400

    first commit

---
+++ assets/js/juego.js
@@ -0,0 +1,43 @@
+/*
+* 2C = Two of Clubs
+* 2D = Two of Diamonds
+* 2H = Two of Hearts
+* 2S = Two of Spades
+*/
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (const tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    }
+
+    for (const tipo of tipos) {
+        for (const especial of especiales) {
+            deck.push(especial + tipo);
+        }
+    }
+
+    deck = _.shuffle(deck);
+    console.log(deck)
+    return deck;
+}
+
+crearDeck();
+
+// Esta función me permite tomar una carta
+const pedirCarta = () => {
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    };
+    const carta = deck.pop();
+    console.log(deck)
+    console.log(carta)
+    return carta;
+}
+
+pedirCarta();
